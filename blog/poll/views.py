18caebfb6f673a8abc46371f2aab7f4657947214
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2022-02-01 15:23:34 +0500

    templates for index,detail view is added

--- blog/poll/views.py
+++ blog/poll/views.py
@@ -1,14 +1,18 @@
-from urllib import response
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from .models import Question
+from django.http import HttpResponse,Http404
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request,'polls/index.html',context)
 
 def detail(request,question_id):
-    response="You are looking at question %s."%question_id
-    return HttpResponse(response)
+    question=get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/detailS.html', {'question': question})
 
 def results(request,question_id):
     response="So this is the result of question id %s"%question_id
