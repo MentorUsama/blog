1753478f1c2815445323d61a88a5f48959f95e9c
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2022-02-02 16:13:30 +0500

    registering admin

--- blog/my_website/admin.py
+++ blog/my_website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from my_website.models.Post import Post
+from my_website.models.Author import Author
+from my_website.models.Tag import Tag
 
 # Register your models here.
+admin.site.register([Post,Author,Tag])
