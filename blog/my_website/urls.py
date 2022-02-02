a17823bb60dc9ebbc4a2a36f8a184bf3540ccee4
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2022-02-02 16:34:30 +0500

    Home page updated

--- blog/my_website/urls.py
+++ blog/my_website/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 app_name = 'my_website'
 urlpatterns=[
-    path("",views.starting_page,name="starting_page"),
+    path("",views.starting_page.as_view(),name="starting_page"),
     path("posts",views.posts,name="posts-page"),
     path("posts/<slug:slug>",views.post_detail,name="post-detail-page")
 ]
--- blog/my_website/views.py
+++ blog/my_website/views.py
@@ -1,9 +1,19 @@
+from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView,DetailView
+from my_website.models.Post import Post
 
 # Create your views here.
-def starting_page(request):
-    return render(request,"my_website/index.html")
+class starting_page(ListView):
+    template_name="my_website/index.html"
+    model=Post
+    ordering=["-date"]
+    context_object_name="posts"
+    def get_queryset(self):
+        querySet=super().get_queryset()
+        data=querySet[:3]
+        return data
 
 def posts(request):
     return HttpResponse("This is posts page")
