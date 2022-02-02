d443b9641072ed953c9f19a4e4744ad8df0c4532
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2022-02-02 14:31:43 +0500

    setting up app name for better url convention

--- blog/my_website/urls.py
+++ blog/my_website/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+app_name = 'my_website'
 urlpatterns=[
     path("",views.starting_page,name="starting_page"),
     path("posts",views.posts,name="posts-page"),
